77130a18568353958816620b1a310ecbfed2cc6f
laptop <acaty2@gmail.com>
2024-06-20 22:26:02 -0500

    test-index.blade.php drag and drop multiple files selector

--- app/Livewire/TestsIndex.php
+++ app/Livewire/TestsIndex.php
@@ -26,10 +26,12 @@ class TestsIndex extends Component
             $path_tmp = '/livewire/preview-file/';
             $start = strpos($value->temporaryUrl(), $path_tmp) + strlen($path_tmp);
             $lenght = strpos($value->temporaryUrl(),'?') - $start;
-            $tmp_file = substr($value->temporaryUrl(), $start, $lenght);
-            $tmp_file = storage_path('app/livewire-tmp') . "/" . $tmp_file;
-            $this->uploaded_files[] = [$filename, $tmp_file, $value->temporaryUrl() ];
+            $tmp_file0 = substr($value->temporaryUrl(), $start, $lenght);
+            $tmp_file_in = storage_path('app/livewire-tmp/' . $tmp_file0);
+            $new_name = 'storage/' . $tmp_file0 . ".pdf";
+            rename($tmp_file_in, $new_name);
+            $this->uploaded_files[] = [$filename, $new_name, $tmp_file_in ];
         }
     }
-
+    
 }
--- resources/views/livewire/tests-index.blade.php
+++ resources/views/livewire/tests-index.blade.php
@@ -1,47 +1,45 @@
-<div class="row">
-    <div class="col col-3">
-        <div
-            class="flex h-screen justify-center items-center"
-            x-data="drop_file_component()">
+<div>
+    <div class="row">
+        <div class="col-3">
             <div
-                class="py-6 w-96 rounded border-dashed border-2 flex flex-col justify-center items-center"
-                x-bind:class="dropingFile ? 'bg-gray-400 border-gray-500' : 'border-gray-500 bg-gray-200'"
-                x-on:drop="dropingFile = false"
-                x-on:drop.prevent="handleFileDrop($event)"
-                x-on:dragover.prevent="dropingFile = true"
-                x-on:dragleave.prevent="dropingFile = false">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z" />
-                </svg>
-                <div class="text-center" wire:loading.remove wire.target="files">Drop Your Files Here</div>
-                <div class="mt-1" wire:loading.flex wire.target="files">
-                    <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-gray-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                class="flex h-screen justify-center items-center"
+                x-data="drop_file_component()">
+                <div
+                    class="py-6 w-96 rounded border-dashed border-2 flex flex-col justify-center items-center"
+                    x-bind:class="dropingFile ? 'bg-gray-400 border-gray-500' : 'border-gray-500 bg-gray-200'"
+                    x-on:drop="dropingFile = false"
+                    x-on:drop.prevent="handleFileDrop($event)"
+                    x-on:dragover.prevent="dropingFile = true"
+                    x-on:dragleave.prevent="dropingFile = false">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z" />
                     </svg>
-                    <div>Processing Files</div>
+                    <div class="text-center" wire:loading.remove wire.target="files">Arrastre sus archivos aquí</div>
+                    <div class="mt-1" wire:loading.flex wire.target="files">
+                        <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-gray-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                        </svg>
+                        <div>Processing Files</div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="col col-6">
-        @if($uploaded_files)
-        <div>
-            @foreach ($uploaded_files as $item)
-                <div class="col col-3">
-                    <li>{{ $item[0] }}</li>
+    @if($uploaded_files)
+    <div class="row">    
+        @foreach ($uploaded_files as $item)
+            <div class="col-6">
+                <div class="row mt-3">
+                    <span>{{ $item[0] }}</span>
                 </div>
-                <div class="col col-3">
-                    <!--a target="_blank" href="{{asset($item[1])}}">Ver PDF</a-->
-                    <!--a target="_blank" href="{{ $item[1] }}">Ver PDF</a-->
-                    <iframe :src="{{ $item[1] }}" style="width: 100%; height:50vw; position: relative; allowfullscreen;"></iframe>
+                <div class="row">
+                    <iframe src="{{ asset($item[1]) }}" style="width: 100%; height:auto; position: relative; allowfullscreen;"></iframe>
                 </div>
-                <!--li>{{ $item[1] }}</li-->
-                <!--li>{{ $item[2] }}</li-->
-            @endforeach
-        </div>
-        @endif
+            </div>
+        @endforeach
     </div>
+    @endif
     <script>
         function drop_file_component() {
             return {
